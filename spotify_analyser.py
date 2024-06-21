c4b0fbc75d79e1f4a089450dae9135aa243a31e7
atancredi <alessandro.tancredi@aitechnologies.it>
2024-06-21 09:52:05 +0200

    updated code logics

--- spotify_analyser.py
+++ spotify_analyser.py
@@ -1,9 +1,6 @@
 import spotipy
 from typing import List
-from os.path import exists
-import argparse
-from json import dump
-from traceback import format_exc
+from dataclasses import dataclass
 
 from spotipy.oauth2 import SpotifyClientCredentials
 from typing import Optional, Any
@@ -11,33 +8,10 @@ from typing import Optional, Any
 ###################
 #   DATA MODELS   #
 ###################
-#REGION
-class Serializable:
-    def as_dict(self, obj = None):
-        """
-        Convert an object or the current instance to a dictionary representation.
-        
-        Args:
-            obj (object, optional): The object to be converted to a dictionary. If not provided, the current instance is used.
-        
-        Returns:
-            dict: A dictionary representation of the object or the current instance.
-        """
-        ret = {}
-        if obj != None:
-            read = obj.__dict__
-        else:
-            read = self.__dict__
+# REGION
 
-        for key in read:
-            value = read[key]
-            if "data." in str(type(value)):
-                ret[key] = self.as_dict(obj=value)
-            else:
-                ret[key] = value
-        return ret
-
-class SongFeatures(Serializable):
+@dataclass
+class SongFeatures:
     danceability: float
     energy: float
     key: int
@@ -57,60 +31,8 @@ class SongFeatures(Serializable):
     duration_ms: int
     time_signature: int
 
-    def from_api_response(self, res: dict):
-        """
-        Initializes the instance variables of the class with the values from the API response.
-
-        Args:
-            res (dict): The API response as a dictionary.
-
-        Returns:
-            self: The instance of the class with the updated variables.
-        """
-        self.danceability = res["danceability"]
-        self.energy = res["energy"]
-        self.key = res["key"]
-        self.loudness = res["loudness"]
-        self.mode = res["mode"]
-        self.speechiness = res["speechiness"]
-        self.acousticness = res["acousticness"]
-        self.instrumentalness = res["instrumentalness"]
-        self.liveness = res["liveness"]
-        self.valence = res["valence"]
-        self.tempo = res["tempo"]
-        self.type = res["type"]
-        self.id = res["id"]
-        self.uri = res["uri"]
-        self.track_href = res["track_href"]
-        self.analysis_url = res["analysis_url"]
-        self.duration_ms = res["duration_ms"]
-        self.time_signature = res["time_signature"]
-        return self
-
-    @property
-    def __dict__(self):
-        return {
-            "danceability": self.danceability,
-            "energy": self.energy,
-            "key": self.key,
-            "loudness": self.loudness,
-            "mode": self.mode,
-            "speechiness": self.speechiness,
-            "acousticness": self.acousticness,
-            "instrumentalness": self.instrumentalness,
-            "liveness": self.liveness,
-            "valence": self.valence,
-            "tempo": self.tempo,
-            "type": self.type,
-            "id": self.id,
-            "uri": self.uri,
-            "track_href": self.track_href,
-            "analysis_url": self.analysis_url,
-            "duration_ms": self.duration_ms,
-            "time_signature": self.time_signature
-        }
-
-class SongData(Serializable):
+@dataclass
+class SongData:
 
     position: Optional[int]
     uri: str
@@ -127,7 +49,6 @@ class SongData(Serializable):
     duration: Optional[float]
     
     def __init__(self):
-        super().__init__()
         self.position = None
         self.uri = ""
         self.name = ""
@@ -141,64 +62,22 @@ class SongData(Serializable):
         self.features = None
         self.duration = None
 
-    @property
-    def __dict__(self):
+    def get_reduced(self):
+        minutes, seconds = divmod(self.features.duration_ms / 1000, 60)
         return {
             "position": self.position,
-            "uri": self.uri,
             "name": self.name,
-            "artist_uri": self.artist_uri,
-            "artist_info": self.artist_info,
             "artist_name": self.artist_name,
-            "artist_pop": self.artist_pop,
-            "artist_genres": self.artist_genres,
             "album": self.album,
-            "track_pop": self.track_pop,
-            "features": self.features,
-            "duration": self.duration
+            "tempo": self.features.tempo,
+            "duration": f'{minutes:0>2.0f}:{seconds:.3f}'
         }
-
-class ReducedSongData(Serializable):
-
-    position: int
-    name: str
-    artist_name: str
-    album: str
-    tempo: float
-    duration: int
-
-    def __init__(self):
-        super().__init__()
-        self.position = None
-        self.name = ""
-        self.artist_name = ""
-        self.album = ""
-        self.tempo = None
-    
-    def from_song_data(self, song_data: SongData):
-        """
-        Initializes the object's attributes with the given song data.
-
-        Parameters:
-            song_data (SongData): The song data used to initialize the object.
-
-        Returns:
-            self: The object itself.
-        """
-        self.position = song_data.position
-        self.name = song_data.name
-        self.artist_name = song_data.artist_name
-        self.album = song_data.album
-        self.tempo = song_data.features["tempo"]
-        minutes, seconds = divmod(song_data.features["duration_ms"] / 1000, 60)
-        self.duration = f'{minutes:0>2.0f}:{seconds:.3f}'
-        return self
-#ENDREGION
+# ENDREGION
 
 ####################
 #   SCAN METHODS   #
 ####################
-#REGION
+# REGION
 
 def get_client(cid,secret) -> spotipy.Spotify:
     #Authentication - without user
@@ -248,7 +127,7 @@ def scan_artist(sp: spotipy.Spotify, artist_uri: str) -> List[SongData]:
             data_item.album = album["name"]
             data_item.artist_name = artist_data["name"]
 
-            data_item.features = SongFeatures().from_api_response(sp.audio_features(track["id"])[0])
+            data_item.features = SongFeatures(**sp.audio_features(track["id"])[0])
 
             song_data.append(data_item)
     return song_data
@@ -265,6 +144,9 @@ def scan_playlist(sp: spotipy.Spotify, playlist_uri: str, reduced = False) -> Li
 
     """
 
+    if sp == None:
+        raise ValueError("Must create a Spotify Client")
+
     ################
     song_data = []
     # READ DATA
@@ -273,67 +155,86 @@ def scan_playlist(sp: spotipy.Spotify, playlist_uri: str, reduced = False) -> Li
         data_item = SongData()
 
         data_item.position = _cnt
-        
-        #URI
+
+        # URI
         data_item.uri = track["track"]["uri"]
-    
-        #Track name
+
+        # Track name
         data_item.name = track["track"]["name"]
-    
-        #Main Artist
+
+        # Main Artist
         data_item.artist_uri = track["track"]["artists"][0]["uri"]
         data_item.artist_info = sp.artist(data_item.artist_uri)
-    
-        #Name, popularity, genre
+
+        # Name, popularity, genre
         data_item.artist_name = track["track"]["artists"][0]["name"]
         data_item.artist_pop = data_item.artist_info["popularity"]
         data_item.artist_genres = data_item.artist_info["genres"]
-    
-        #Album
+
+        # Album
         data_item.album = track["track"]["album"]["name"]
-    
-        #Popularity of the track
+
+        # Popularity of the track
         data_item.track_pop = track["track"]["popularity"]
-                
+
         features = sp.audio_features(data_item.uri)[0]
         # Half tempo if over 135BPM
         if features["tempo"] > 135.0:
             features["tempo"] = features["tempo"] / 2
-        
-        data_item.features = SongFeatures().from_api_response(features)
+
+        data_item.features = SongFeatures(**features)
 
         data_item.duration = divmod(data_item.features.duration_ms / 1000, 60)
 
         if reduced:
-            song_data.append(ReducedSongData().from_song_data(data_item))
+            song_data.append(data_item.get_reduced())
         else:
             song_data.append(data_item)
 
         _cnt += 1
-        
+
     return song_data
-#ENDREGION
+# ENDREGION
 
 ##############################
 #   COMMAND LINE INTERFACE   #
 ##############################
-#REGION
+# REGION
 
 if __name__ == "__main__":
 
+    import argparse
     from dotenv import load_dotenv
     from os import environ as env, mkdir
+    from os.path import exists
+    from json import dump
+    from traceback import format_exc
+
     load_dotenv()
-    
+
     parser = argparse.ArgumentParser()
-    parser.add_argument("-m","--mode", type=str, default="playlist", help="Scan mode (playlist, artist)")
-    parser.add_argument("-i","--id", type=str, required=True, help="Playlist URI from Spotify")
-    parser.add_argument("-o","--output", type=str, help="Name for the output file", default="output.json")
-    parser.add_argument("--reduced", help="Reduced output", action='store_true')
-    
+    parser.add_argument(
+        "-m",
+        "--mode",
+        type=str,
+        default="playlist",
+        help="Scan mode (playlist, artist)",
+    )
+    parser.add_argument(
+        "-i", "--id", type=str, required=True, help="Playlist URI from Spotify"
+    )
+    parser.add_argument(
+        "-o",
+        "--output",
+        type=str,
+        help="Name for the output file",
+        default="output.json",
+    )
+    parser.add_argument("--reduced", help="Reduced output", action="store_true")
+
     try:
         args = parser.parse_args()
-        
+
         cid = env.get("CLIENT_ID")
         secret = env.get("SECRET_KEY")
         sp = get_client(cid, secret)
@@ -346,16 +247,16 @@ if __name__ == "__main__":
             case "artist":
                 print(f"Scanning artist {args.id}...")
                 res = scan_artist(sp, args.id)
-        
+
         if not exists("results"):
             mkdir("results")
         if not exists(f"results/{args.mode}"):
             mkdir(f"results/{args.mode}")
-        dump([i.__dict__ for i in res],open(f"results/{args.mode}/{args.output}", "w"), indent=4)
+        dump(res, open(f"results/{args.mode}/{args.output}", "w"), indent=4)
 
         print("Done")
     except Exception as ex:
-        print("Error: "+str(ex))
+        print("Error: " + str(ex))
         print(format_exc())
 
-#ENDREGION
+# ENDREGION
